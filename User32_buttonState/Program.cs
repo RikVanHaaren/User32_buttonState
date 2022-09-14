61f71ecd1dfb7bc585145de7fd28d9ff186253fc
Rik van Haaren <486859@student.fontys.nl>
2022-09-14 20:24:57 +0200

    Add example

---
+++ User32_buttonState/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+using User32_buttonState;
+
+// Import P/Invoke
+[DllImport("USER32.dll")]
+static extern short GetKeyState(VirtualKey nVirtKey);
+
+// KEY_TOGGLED = &H1 (0x1)
+// KEY_PRESSED = &H8000 (0x8000)
+const int KEY_PRESSED = 0x8000;
+
+while (true) {
+    if (IsPressed(VirtualKey.VK_SPACE)) Console.WriteLine("Button Pressed!");
+    Thread.Sleep(100);
+}
+
+static bool IsPressed(VirtualKey virtualKey) {
+    return Convert.ToBoolean(GetKeyState(virtualKey) & KEY_PRESSED);
+}
